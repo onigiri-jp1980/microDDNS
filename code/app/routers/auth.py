2ae4d1ee5c04ecc9b1e86de0d96322eb7dd2453c
onigiri-jp1980 <d.yamashiro.mob@gmail.com>
2026-02-23 11:38:20 +0900

    認証レスポンススキーマを追加

--- code/app/routers/auth.py
+++ code/app/routers/auth.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter
 from app.services.auth import CognitoService
-from app.models.auth import AuthRequest
+from app.models.auth import AuthRequest, AuthResponse
 auth_router = APIRouter(prefix="/auth")
 
 @auth_router.post("/login")
-def sign_in(request: AuthRequest):
+def sign_in(request: AuthRequest)->AuthResponse:
     return CognitoService(email=request.email).sign_in(request.email, request.password)
