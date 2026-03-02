e36a63658daa067e6c1fa72f3f5ccc18f3126391
onigiri-jp1980 <d.yamashiro.mob@gmail.com>
2026-03-02 15:34:00 +0900

    作業退避

--- code/backend/app/services/cloudflare.py
+++ code/backend/app/services/cloudflare.py
@@ -15,12 +15,25 @@ def get_client():
 class DNSManagementService:
     def __init__(self):
         self.client = get_client()
+        self.zones = [{'id': zone.id, **zone.to_dict()}
+                      for zone in self.get_zones()]
 
     def get_zones(self):
-        return self.client.zones.list()
+        return self.client.zones.list().result
 
     def get_zone(self, zone_id):
         return self.client.zones.get(zone_id)
 
     def get_records(self, zone_id):
-        return self.client.zones.get(zone_id).dns_records.get()
+        _zones = self.client.dns.records.list(zone_id=zone_id).result
+        return [{'id': record.id, **record.to_dict()}
+                for record in _zones]
+
+    def get_record(self, zone_id, record_id):
+        return self.client.dns.records.get(zone_id=zone_id, record_id=record_id)
+
+    def create_record(self, zone_id, record):
+        return self.client.dns.records.create(zone_id=zone_id, record=record)
+
+    def update_record(self, zone_id, record_id, record):
+        return self.client.dns.records.update(zone_id=zone_id, record_id=record_id, record=record)
