27822fc921226dd823a39cadee5e67cf4d2e4b9f
onigiri-jp1980 <d.yamashiro.mob@gmail.com>
2026-03-28 11:00:22 +0900

    kumo一本化

--- code/backend/create_user_cognito.py
+++ code/backend/create_user_cognito.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python3
 from app.services.auth import CognitoService as Cognito
 from argparse import ArgumentParser, RawTextHelpFormatter
+from boto3 import Session
+from botocore import client as botocoreClient
 from os import environ as env
 
 defaults = {
@@ -8,8 +10,10 @@ defaults = {
     'region': 'ap-northeast-1' or env.get('AWS_REGION', 'ap-northeast-1'),
     'email': 'test@example.com',
     'password': 'pa55word!',
-    'cognito': 'floci' or env.get('COGNITO_NAME', 'floci'),
+    'cognito': 'floci' or env.get('COGNITO_NAME', 'kumo'),
     'user-pool': 'local' or env.get('COGNITO_USER_POOL', 'local'),
+    'client-id': 'local' or env.get('COGNITO_CLIENT_ID', 'local'),
+    'client-secret': 'local' or env.get('COGNITO_CLIENT_SECRET', 'local'),
 }
 
 
@@ -17,25 +21,45 @@ help_descriptions = {
     'help': 'Cognito上にユーザーを作成する',
     'email': 'ユーザーのEmail',
     'password': 'ユーザーのパスワード',
-    'cognito': '作成先のCognito (floci: デフォルト, aws: AWS上)',
-    'user-pool': '作成先のユーザープール名 (デフォルト: local)',
-    'profile': '作成先のAWSプロファイル名 (デフォルト: default)',
-    'region': '作成先のAWSリージョン (デフォルト: ap-northeast-1)',
+    'cognito': '作成先のCognito (デフォルト kumo, aws: AWS上)',
+    'user-pool': '作成先のユーザープール名 (デフォルト: localまたは環境変数COGNITO_USER_POOLから取得)',
+    'profile': '作成先のAWSプロファイル名 (デフォルト: defaultまたは環境変数AWS_PROFILEから取得)',
+    'region': '作成先のAWSリージョン (デフォルト: ap-northeast-1または環境変数AWS_REGIONから取得)',
+    'client-id': '作成先のCognitoクライアントID (デフォルト: localまたは環境変数COGNITO_CLIENT_IDから取得)',
+    'client-secret': '作成先のCognitoクライアントシークレット (デフォルト: localまたは環境変数COGNITO_CLIENT_SECRETから取得)',
 }
 
 def parse_args():
     parser = ArgumentParser(formatter_class=RawTextHelpFormatter, description=help_descriptions['help'])
     parser.add_argument('--email', '-e', type=str, default=defaults['email'], help=help_descriptions['email'])
     parser.add_argument('--password', '-p', type=str, default=defaults['password'], help=help_descriptions['password'])
+    parser.add_argument('--client-id', '-i', type=str, default=defaults['client-id'], help=help_descriptions['client-id'])
+    parser.add_argument('--client-secret', '-s', type=str, default=defaults['client-secret'], help=help_descriptions['client-secret'])
     parser.add_argument('--cognito', '-c', type=str, default=defaults['cognito'], help=help_descriptions['cognito'])
     parser.add_argument('--user-pool', '-u', type=str, default=defaults['user-pool'], help=help_descriptions['user-pool'])
-    parser.add_argument('--profile', '-p', type=str, default=defaults['profile'], help=help_descriptions['profile'])
+    parser.add_argument('--profile', '-f', type=str, default=defaults['profile'], help=help_descriptions['profile'])
     parser.add_argument('--region', '-r', type=str, default=defaults['region'], help=help_descriptions['region'])
     return parser.parse_args()
 
 def create_user_cognito(args):
-    cognito = Cognito()
-    cognito.create_user(args.email, args.password)
+    cognito = get_cognito_client(args)
+    user = cognito.admin_create_user(
+        UserPoolId=args.user_pool,
+        UserAttributes=[
+            {'Name': 'email', 'Value': args.email},
+            {'Name': 'email_verified', 'Value': 'true'},
+            ]
+        Username=args.email,
+        MessageAction='SUPPRESS',
+    )['User']
+    
+
+
+def get_cognito_client(args):
+    return Session(
+        profile_name=args.profile if args.cognito == 'aws' else None, 
+        region_name=args.region if args.cognito == 'aws' else None,
+        ).client('cognito-idp')
 
 def main():
     args = parse_args()
